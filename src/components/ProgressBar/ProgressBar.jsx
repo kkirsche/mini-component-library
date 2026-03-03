1e698635c7db4dc07357b9f3d8b4dd2bf66588d6
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2026-03-03 14:30:14 -0500

    Add non-native progress bar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -6,6 +6,7 @@ import { COLORS } from "../../constants";
  * @typedef {object} ProgressBarProps
  * @property {"small" | "medium" | "large"} size - The progress bar size.
  * @property {number} size - The value (0–100, inclusive) of the progress bar.
+ * @property {boolean} native - Use the native <progress> element.
  */
 
 const STYLES = {
@@ -40,7 +41,6 @@ const Progress = styled.progress`
     border-radius: var(--outer-border-radius);
   }
 
-  /* 4. Kill Chrome's default blue gradient */
   &::-webkit-progress-value {
     background-color: ${COLORS.primary};
     border-top-left-radius: var(--inner-border-radius);
@@ -50,17 +50,44 @@ const Progress = styled.progress`
   }
 `;
 
+const ProgressWrapper = styled.div`
+    height: var(--height);
+    background: ${COLORS.transparentGray15};
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    padding: var(--padding);
+    border-radius: var(--outer-border-radius);
+    overflow: hidden;
+`;
+
+const Trimmer = styled.div`
+  border-radius: var(--inner-border-radius);
+  overflow: hidden;
+  height: 100%;
+`
+
+const ProgressValue = styled.div`
+    height: 100%;
+    width: ${(props) => props.value}%;
+    background-color: ${COLORS.primary};
+`;
+
 /**
  * A progress bar.
  * @param {ProgressBarProps} props The progress bar props.
  * @returns {JSX.Element} A React element displaying the progress bar.
  */
-const ProgressBar = ({ value, size }) => {
-  return (
+const ProgressBar = ({ value, size, native = false}) => {
+  return native ? (
     <label>
     <Progress max="100" value={value} style={{...STYLES[size]}}>
       {value}%
     </Progress></label>
+  ) : (
+    <ProgressWrapper style={{...STYLES[size]}} role="progressbar" aria-label="Progress Bar" aria-valuenow={value} aria-valuemin={0} aria-valuemax={100}>
+      <Trimmer style={{...STYLES[size]}}>
+        <ProgressValue style={{...STYLES[size]}} value={value} />
+      </Trimmer>
+    </ProgressWrapper>
   );
 };
 
